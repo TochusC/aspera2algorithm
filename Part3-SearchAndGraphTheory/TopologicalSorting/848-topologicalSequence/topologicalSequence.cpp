c848ea8f6545f23e8e1ce8624a444021a065dbfe
TochusC <205329624@qq.com>
2024-05-23 16:29:41 +0800

    Add 848-topologicalSequence.

--- Part3-SearchAndGraphTheory/TopologicalSorting/848-topologicalSequence/topologicalSequence.cpp
+++ Part3-SearchAndGraphTheory/TopologicalSorting/848-topologicalSequence/topologicalSequence.cpp
@@ -1,3 +1,46 @@
-//
-// Created by 20532 on 2024/5/23.
-//
+#include <cstdio>
+#include <vector>
+#include <queue>
+
+#define MAX 100010
+
+using namespace std;
+
+vector<int> eg[MAX];
+queue<int> que;
+
+int path[MAX];
+int in[MAX];
+bool out[MAX];
+int len = 0;
+
+int n, m;
+
+int main(){
+    scanf("%d %d", &n, &m);
+
+    int from, to;
+    while(m--){
+        scanf("%d %d", &from, &to);
+        eg[from].push_back(to);
+        in[to]++;
+    }
+
+    while(true){
+        for(int i = 1; i <= n; i++){
+            if(!in[i] && !out[i]) que.push(i);
+        }
+        if(que.empty()) break;
+        else while(!que.empty()){
+            int nd = que.front();
+            que.pop();
+            for(auto e : eg[nd]) in[e]--;
+            path[len++] = nd;
+            out[nd] = true;
+        }
+    }
+
+    if(len == n) for(int i = 0; i < len; i++) printf("%d ", path[i]);
+    else printf("-1");
+
+}
