86f60d6bba606e9d53d06c0981903c19d9481f42
TochusC <205329624@qq.com>
2024-05-24 11:38:13 +0800

    Add 861-maximumMatch.

--- Part3-SearchAndGraphTheory/HungarianAlgorithm/861-maximumMatch/maximumMatch.cpp
+++ Part3-SearchAndGraphTheory/HungarianAlgorithm/861-maximumMatch/maximumMatch.cpp
@@ -1,3 +1,43 @@
-//
-// Created by 20532 on 2024/5/24.
-//
+#include <cstdio>
+#include <vector>
+#include <utility>
+
+#define NMAX 550
+
+using namespace std;
+
+vector<int> eg[NMAX];
+bool sel[NMAX];
+
+int match[NMAX];
+
+bool eureka(int nd){
+    for(auto &e : eg[nd]){
+        if(sel[e]) continue;
+        sel[e] = true;
+        if(!match[e] || eureka(match[e])) {
+            match[e] = nd;
+            return true;
+        }
+    }
+    return false;
+}
+
+int main(){
+    int n1, n2, m;
+    scanf("%d %d %d", &n1, &n2, &m);
+
+    int from, to;
+    while(m--){
+        scanf("%d %d", &from, &to);
+        eg[from].push_back(to);
+    }
+
+    int res = 0;
+    for(int i = 1; i <= n1; i++){
+        for(auto &e:sel) e = false;
+        if(eureka(i)) res++;
+    }
+
+    printf("%d\n", res);
+}
