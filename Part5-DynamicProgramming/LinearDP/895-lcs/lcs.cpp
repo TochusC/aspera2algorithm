963fba24e5a513f80c69d690493a651e1aed2e9b
TochusC <205329624@qq.com>
2024-05-24 20:40:35 +0800

    Add 895-lcs.

---
+++ Part5-DynamicProgramming/LinearDP/895-lcs/lcs.cpp
@@ -0,0 +1,32 @@
+#include <cstdio>
+#include <algorithm>
+#include <array>
+
+#define NMAX 1010
+#define INF 2e9
+
+using namespace std;
+
+array<int, NMAX> dp;
+
+int main(){
+    for(auto &e :dp)
+        e = INF;
+
+    int n;
+    scanf("%d", &n);
+
+    int num;
+    scanf("%d", &num);
+    dp[1] = num;
+
+    int rst = 1;
+    for(int i = 1; i < n; i++){
+        scanf("%d", &num);
+        auto low = lower_bound(dp.begin() + 1, dp.begin() + n + 1, num);
+        int len = low - dp.begin();
+        dp[len] = num;
+        rst = max(rst, len);
+    }
+    printf("%d", rst);
+}
